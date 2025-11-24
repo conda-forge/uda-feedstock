65dfb1ce66348cad49ba3a08f2043880eee9a3e4
munechika-koyo <munechika.koyo@gmail.com>
2025-11-24 09:19:52 +0100

    Fix Java executable exports in build_java.sh for cross-compilation

--- recipe/scripts/build_java.sh
+++ recipe/scripts/build_java.sh
@@ -5,12 +5,14 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:
   export CAPNP_EXECUTABLE="${PREFIX}/bin/capnp"
   export CAPNPC_CXX_EXECUTABLE="${PREFIX}/bin/capnpc-c++"
   export Java_JAVA_EXECUTABLE="${PREFIX}/lib/jvm/bin/java"
+  export Java_JAVAC_EXECUTABLE="${PREFIX}/lib/jvm/bin/javac"
+  export Java_JAVAH_EXECUTABLE="${PREFIX}/lib/jvm/bin/java"
 else
   export CAPNP_EXECUTABLE="${BUILD_PREFIX}/bin/capnp"
   export CAPNPC_CXX_EXECUTABLE="${BUILD_PREFIX}/bin/capnpc-c++"
   export Java_JAVA_EXECUTABLE="${BUILD_PREFIX}/bin/java"
   export Java_JAVAC_EXECUTABLE="${BUILD_PREFIX}/bin/javac"
-  export Java_JAVAH_EXECUTABLE="${BUILD_PREFIX}/bin/javah"
+  export Java_JAVAH_EXECUTABLE="${BUILD_PREFIX}/bin/java"
 fi
 
 # CMake extra configuration:
